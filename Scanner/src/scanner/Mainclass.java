dfc3f980f22e4aa6abc177cf041ea23964d99949
ELCOT <ELCOT@Lenovo>
2021-03-26 09:12:33 +0530

    Scanner

---
+++ Scanner/src/scanner/Mainclass.java
@@ -0,0 +1,15 @@
+package scanner;
+
+import java.util.Scanner;
+
+public class Mainclass {
+
+	public static void main(String[] args) {
+ 
+		int num;
+		System.out.println("Enter the Integer number :");
+		Scanner sc=new Scanner(System.in);
+		 num=sc.nextInt();
+		  System.out.println("Enter the number by user :"+num);
+		}
+}
